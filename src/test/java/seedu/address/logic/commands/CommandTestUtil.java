e2e9271486e6bbcb29781bb4b9a82b22785a9938
Wilkins Ang <angweijian2001@gmail.com>
2025-03-24 22:24:30 +0800

    Update Tests to be inline with Edit

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -40,10 +40,8 @@ public class CommandTestUtil {
     public static final String VALID_PHONE_BOB = "22222222";
     public static final String VALID_EMAIL_AMY = "amy@example.com";
     public static final String VALID_EMAIL_BOB = "bob@example.com";
-    //public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_JOBPOSITION_AMY = "Hiring Womanager";
     public static final String VALID_JOBPOSITION_BOB = "Hiring Manager";
-    //public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
     public static final LocalDate VALID_BIRTHDAY_DATE_AMY = LocalDate.of(1995, 3, 16);
