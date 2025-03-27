082dbf2ae9ce96f3a4798d8f7048ab659f3d9f79
Wilkins Ang <angweijian2001@gmail.com>
2025-03-27 08:59:07 +0800

    Update Tests with Mockito

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,32 +1,37 @@
 package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
-import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Predicate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
-import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.person.EmployeeId;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 public class FindCommandParserTest {
 
+    private Model model;
     private FindCommandParser parser = new FindCommandParser();
 
+    @BeforeEach
+    public void setUp() {
+        model = Mockito.mock(Model.class);
+    }
     @Test
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ",
@@ -42,22 +47,32 @@ public class FindCommandParserTest {
         Person matching2 = new PersonBuilder().withName("Bob Marley").build();
         Person nonMatching = new PersonBuilder().withName("Charlie Brown").build();
 
-        ModelStubWithFilterablePersons model =
-                new ModelStubWithFilterablePersons(List.of(matching1, matching2, nonMatching));
+        ObservableList<Person> baseList =
+                FXCollections.observableArrayList(matching1, matching2, nonMatching);
+        FilteredList<Person> filteredList = new FilteredList<>(baseList);
+
+        //stub creation
+        when(model.getFilteredPersonList()).thenReturn(filteredList);
+
+        doAnswer(invocation -> {
+            Predicate<Person> predicate = invocation.getArgument(0);
+            filteredList.setPredicate(predicate);
+            return null;
+        }).when(model).updateFilteredPersonList(any());
 
         command.execute(model);
 
-        assertEquals(List.of(matching1, matching2), model.getFilteredPersonList());
+        assertEquals(List.of(matching1, matching2), filteredList);
+
+        //reset list
+        filteredList.setPredicate(unused -> true);
 
         // multiple whitespaces between keywords
         FindCommand whitespaceCommand = parser.parse(" \n n/Alice \n \t Bob  \t");
 
-        ModelStubWithFilterablePersons whitespaceModel =
-                new ModelStubWithFilterablePersons(List.of(matching1, matching2, nonMatching));
+        whitespaceCommand.execute(model);
 
-        whitespaceCommand.execute(whitespaceModel);
-
-        assertEquals(List.of(matching1, matching2), whitespaceModel.getFilteredPersonList());
+        assertEquals(List.of(matching1, matching2), model.getFilteredPersonList());
     }
 
     @Test
@@ -69,22 +84,31 @@ public class FindCommandParserTest {
         Person matching2 = new PersonBuilder().withJobPosition("Product Manager").build();
         Person nonMatching = new PersonBuilder().withJobPosition("Sales Associate").build();
 
-        ModelStubWithFilterablePersons model =
-                new ModelStubWithFilterablePersons(List.of(matching1, matching2, nonMatching));
+        ObservableList<Person> baseList =
+                FXCollections.observableArrayList(matching1, matching2, nonMatching);
+        FilteredList<Person> filteredList = new FilteredList<>(baseList);
+
+        when(model.getFilteredPersonList()).thenReturn(filteredList);
+
+        doAnswer(invocation -> {
+            Predicate<Person> predicate = invocation.getArgument(0);
+            filteredList.setPredicate(predicate);
+            return null;
+        }).when(model).updateFilteredPersonList(any());
 
         command.execute(model);
 
         assertEquals(List.of(matching1, matching2), model.getFilteredPersonList());
 
+        //reset list
+        filteredList.setPredicate(unused -> true);
+
         // Parse command with messy whitespace
         FindCommand whitespaceCommand = parser.parse(" \n jp/engineer \t manager  \n");
 
-        ModelStubWithFilterablePersons whitespaceModel =
-                new ModelStubWithFilterablePersons(List.of(matching1, matching2, nonMatching));
+        whitespaceCommand.execute(model);
 
-        whitespaceCommand.execute(whitespaceModel);
-
-        assertEquals(List.of(matching1, matching2), whitespaceModel.getFilteredPersonList());
+        assertEquals(List.of(matching1, matching2), model.getFilteredPersonList());
     }
 
     @Test
@@ -112,143 +136,30 @@ public class FindCommandParserTest {
                 .withJobPosition("Accountant")
                 .build();
 
-        ModelStubWithFilterablePersons model =
-                new ModelStubWithFilterablePersons(List.of(matching, wrongName, wrongJob, completelyOff));
+        ObservableList<Person> baseList =
+                FXCollections.observableArrayList(matching, wrongName, wrongJob, completelyOff);
+        FilteredList<Person> filteredList = new FilteredList<>(baseList);
+
+        when(model.getFilteredPersonList()).thenReturn(filteredList);
+
+        doAnswer(invocation -> {
+            Predicate<Person> predicate = invocation.getArgument(0);
+            filteredList.setPredicate(predicate);
+            return null;
+        }).when(model).updateFilteredPersonList(any());
 
         command.execute(model);
 
         assertEquals(List.of(matching), model.getFilteredPersonList());
 
+        //reset list
+        filteredList.setPredicate(unused -> true);
+
         // Messy whitespace version
         FindCommand messyCommand = parser.parse(" \n n/jack  \n jp/engineer \t");
 
-        ModelStubWithFilterablePersons messyModel =
-                new ModelStubWithFilterablePersons(List.of(matching, wrongName, wrongJob, completelyOff));
+        messyCommand.execute(model);
 
-        messyCommand.execute(messyModel);
-
-        assertEquals(List.of(matching), messyModel.getFilteredPersonList());
+        assertEquals(List.of(matching), model.getFilteredPersonList());
     }
-
-
-    /**
-     * A default model stub that have all the methods failing.
-     */
-    private class ModelStub implements Model {
-        @Override
-        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyUserPrefs getUserPrefs() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public GuiSettings getGuiSettings() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setGuiSettings(GuiSettings guiSettings) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public Path getAddressBookFilePath() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setAddressBookFilePath(Path addressBookFilePath) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void addPerson(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setAddressBook(ReadOnlyAddressBook newData) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyAddressBook getAddressBook() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean hasPerson(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean hasEmployeeIdPrefixConflict(EmployeeId employeeId) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean hasEmployeeIdPrefixConflictIgnoringSpecific(EmployeeId employeeId, EmployeeId toIgnore) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean hasDuplicatePersonDetails(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void deletePerson(Person target) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setPerson(Person target, Person editedPerson) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ObservableList<Person> getFilteredPersonList() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ObservableList<Person> getFilteredByEmployeeIdPrefixList(EmployeeId employeeIdPrefix) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void updateFilteredPersonList(Predicate<Person> predicate) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void commitChanges() {
-            // Stub implementation, no-op
-        }
-    }
-
-    private class ModelStubWithFilterablePersons extends ModelStub {
-        private final ObservableList<Person> persons = FXCollections.observableArrayList();
-        private final FilteredList<Person> filteredPersons;
-
-        ModelStubWithFilterablePersons(List<Person> initialPersons) {
-            persons.addAll(initialPersons);
-            filteredPersons = new FilteredList<>(persons);
-        }
-
-        @Override
-        public void updateFilteredPersonList(Predicate<Person> predicate) {
-            filteredPersons.setPredicate(predicate);
-        }
-
-        @Override
-        public ObservableList<Person> getFilteredPersonList() {
-            return filteredPersons;
-        }
-    }
-
 }
