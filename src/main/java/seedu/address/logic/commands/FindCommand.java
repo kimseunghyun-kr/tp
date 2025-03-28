fa99036277804d1f60222833642223e0e9acfd05
Wilkins Ang <angweijian2001@gmail.com>
2025-03-28 17:59:48 +0800

    Update Message Usage to be clearer with combined search

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -24,8 +24,12 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE =
             COMMAND_WORD + ": Finds all persons whose names or job position contain any of "
                     + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+                    + "If both name and job position keywords are provided, "
+                    + "then only employees with both matching name and job position will be listed.\n"
                     + "Parameters for Name search: " + PREFIX_NAME + "KEYWORD [MORE_KEYWORDS]...\n"
                     + "Parameters for Position search: " + PREFIX_JOBPOSITION + "KEYWORD [MORE_KEYWORDS]...\n"
+                    + "Parameters for Combined search: " +  PREFIX_NAME + "KEYWORD [MORE_KEYWORDS]... "
+                    + PREFIX_JOBPOSITION + "KEYWORD [MORE_KEYWORDS]...\n"
                     + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + " alice bob charlie";
 
     private final Predicate<Person> predicate;
